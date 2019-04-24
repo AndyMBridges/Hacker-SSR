18fbadb8ef740ad867eb5083eb1a32db8ed0ec91
Andy Bridges <andy.bridges@publicissapient.com>
2019-04-24 13:54:23 +0100

    force https on api

--- src/actions/postsActions.ts
+++ src/actions/postsActions.ts
@@ -1,27 +1,27 @@
 // Import redux types
-import { ActionCreator, Dispatch } from 'redux';
-import { ThunkAction } from 'redux-thunk';
-import axios from 'axios';
+import { ActionCreator, Dispatch } from 'redux'
+import { ThunkAction } from 'redux-thunk'
+import axios from 'axios'
 
 // Import Post Typing
-import { IPost, IPostState } from '../reducers/postsReducer';
+import { IPost, IPostState } from '../reducers/postsReducer'
 
 // Create Action Constants
 export enum PostActionTypes {
-  GET_ALL = 'GET_ALL',
+  GET_ALL = 'GET_ALL'
 }
 
 // Interface for Get All Action Type
 export interface IPostGetAllAction {
-  type: PostActionTypes.GET_ALL;
-  posts: IPost[];
+  type: PostActionTypes.GET_ALL
+  posts: IPost[]
 }
 
 /* 
 Combine the action types with a union (we assume there are more)
 example: export type PostActions = IGetAllAction | IGetOneAction ... 
 */
-export type PostActions = IPostGetAllAction;
+export type PostActions = IPostGetAllAction
 
 /* Get All Action
 <Promise<Return Type>, State Interface, Type of Param, Type of Action> */
@@ -30,16 +30,17 @@ export const getAllPosts: ActionCreator<
 > = query => {
   return async (dispatch: Dispatch) => {
     try {
+      const searchTerm = query ? query : 'typescript'
 
-      const searchTerm = query ? query : 'typescript';
-
-      const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${searchTerm}`);
+      const response = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${searchTerm}`
+      )
       dispatch({
         posts: response.data.hits,
-        type: PostActionTypes.GET_ALL,
-      });
+        type: PostActionTypes.GET_ALL
+      })
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
-};
+  }
+}
